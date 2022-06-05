cc40fc431412886d584e1cc8186ff71ead162c8a
Mezaya <mesaya@live.com>
2022-06-05 16:40:18 +0200

    Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
+ */
+int main(void){
+    int alph;
+    for(alph = 'a'; alph <= 'z'; alph++){
+        putchar(alph);
+        putchar('\n');
+    }
+return (0);
+}
