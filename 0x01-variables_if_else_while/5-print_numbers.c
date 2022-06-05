cd48d85df62481c7889544a72a09d49d861cf6b0
Mezaya <mesaya@live.com>
2022-06-05 18:20:20 +0200

    Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digit numbers of base 10.
+ * Return: 0
+ */
+int main(void)
+{
+int i =0;
+for (int i =0; i != 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
