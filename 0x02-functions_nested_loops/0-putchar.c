545e29c5d32ecb921a0f546212d1299b4ce51657
Mezaya <mesaya@live.com>
2022-06-07 20:09:03 +0200

    Variables

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*
+ * main - program that prints _putchar.
+ * Return: 0
+ */
+int main(void)
+{
+char tex[9] = "_putchar";
+int i = 0;
+for (i = 0; i < 8; i++)
+{
+putchar(tex[i]);
+}
+putchar('\n');
+return (0);
+}
