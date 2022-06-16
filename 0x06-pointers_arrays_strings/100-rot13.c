dede83a08682e4423a0501ada35c1365542a5f7b
Mezaya <mesaya@live.com>
2022-06-16 21:06:22 +0200

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rot13 - encodes strings using rot13.
+ * @s: pointer to string.
+ * Return: pointer to encoded string.
+ */
+char *rot13(char *s)
+{
+int a = 0;
+while (s[a])
+{
+while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+{
+if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+{
+s[a] -= 13;
+break;
+}
+s[a] += 13;
+break;
+}
+a++;
+}
+return (s);
+}
