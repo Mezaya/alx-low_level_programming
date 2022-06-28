d141291c25c2b4a0f77c746114219f11a61c274d
Mezaya <mesaya@live.com>
2022-06-28 22:46:19 +0200

    malloc, alloc, calloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,24 +8,24 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-char *strout;
-unsigned int i, j, k, limit;
+int size1, size2, i;
+char *m;
 if (s1 == NULL)
-s1 = "";
+s1 = "\0";
 if (s2 == NULL)
-s2 = "";
-for (i = 0; s1[i] != '\0'; i++);
-for (j = 0; s2[j] != '\0'; j++);
-strout = malloc(sizeof(char) * (i + j + 1));
-if (strout == NULL)
+s2 = "\0";
+size1 = _strlen(s1);
+size2 = _strlen(s2);
+m = malloc((size1 + size2) *sizeof(char) + 1);
+if (m == 0)
+return (0);
+for (i = 0; i <= size1 + size2; i++)
 {
-free(strout);
-return (NULL);
+if (i < size1)
+m[i] = s1[i];
+else
+m[i] = s2[i - size1];
 }
-for (k = 0; k < i; k++);
-strout[k] = s1[k];
-limit = j;
-for (j = 0; j <= limit; k++, j++);
-strout[k] = s2[j];
-return (strout);
+m[i] = '\0';
+return (m);
 }
