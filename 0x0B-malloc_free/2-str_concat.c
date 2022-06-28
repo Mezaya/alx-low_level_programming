e5c6566a3e9678ef4b0d194043e610721f006ba3
Mezaya <mesaya@live.com>
2022-06-28 21:44:12 +0200

    malloc, alloc, calloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,7 +4,6 @@
 * str_concat - concatenates two strings.
 * @s1: first string.
 * @s2: second string.
-*
 * Return: pointer of an array of chars
 */
 char *str_concat(char *s1, char *s2)
@@ -23,10 +22,10 @@ if (strout == NULL)
 free(strout);
 return (NULL);
 }
-for (k = 0; k < i; k++)
+for (k = 0; k < i; k++);
 strout[k] = s1[k];
 limit = j;
-for (j = 0; j <= limit; k++, j++)
+for (j = 0; j <= limit; k++, j++);
 strout[k] = s2[j];
 return (strout);
 }
