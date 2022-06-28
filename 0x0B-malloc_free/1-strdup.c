b3b3fb409f216e221e732b1ceabf2d0bea03c97b
Mezaya <mesaya@live.com>
2022-06-28 22:19:31 +0200

    malloc, alloc, calloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,13 +14,13 @@ int i = 0, size = 0;
 char *m;
 if (str == NULL)
 return (NULL);
-for (; str[size] != '\0'; size++);
+for (str[size] != '\0'; size++);
 m = malloc(size * sizeof(*str) + 1);
 if (m == 0)
 return (NULL);
 else
 {
-for (; i < size; i++)
+for (i < size; i++)
 m[i] = str[i];
 }
 return (m);
