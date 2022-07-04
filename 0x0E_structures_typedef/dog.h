f493fc697951e7b6ff6fdc6fb6311856944dba09
Mezaya <mesaya@live.com>
2022-07-04 22:24:47 +0200

    Structure, typing_def.

--- 0x0E_structures_typedef/dog.h
+++ 0x0E_structures_typedef/dog.h
@@ -1,21 +1,21 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _dog_h_
+#define _dog_h_
 /**
-* struct dog - Struct dog
-* @name: name
-* @age: age
-* @owner:  owner
-* Description: This struct is for dog
-*/
-struct dog
+ * struct dog - such structure, wow
+*  @name: dog's name
+ *  @age: dog's age
+*  @owner: dog's owner
+ */
+typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-};
-typedef struct dog dog_t;
+}
+dog_t;
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /* _dog_h_ */
