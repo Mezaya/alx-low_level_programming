febccb57b244080e3841d8d3f139b82ea98ea54c
Mezaya <mesaya@live.com>
2022-07-07 21:52:48 +0200

    Variadick Functionz

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,22 +1,26 @@
 #include "variadic_functions.h"
-5/**
-* print_numbers - prints numbers.
- * @separator: string to be printed between numbers.
-* @n: number of integers passed to the function.
- *
-* Return: no return.
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - Entry Point
+* @separator: comma space
+ * @n: elements to be printed
+*  Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list valist;
+char *sep;
 unsigned int i;
-va_start(valist, n);
-for (i = 0; i < n; i++)
-{
-printf("%d", va_arg(valist, int));
-if (separator && i < n - 1)
-printf("%s", separator);
-}
+va_list list;
+if (separator == NULL || *separator == 0)
+sep = "";
+else
+sep = (char *) separator;
+va_start(list, n);
+if (n > 0)
+printf("%d", va_arg(list, int));
+for (i = 1; i < n; i++)
+printf("%s%d", sep, va_arg(list, int));
 printf("\n");
-va_end(valist);
+va_end(list);
 }
